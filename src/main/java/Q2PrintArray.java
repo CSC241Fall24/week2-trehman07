176ba74966b574de1814fb7c5ddfba48d4859c2e
trehman07 <147639134+trehman07@users.noreply.github.com>
2024-09-10 15:02:28 -0400

    Completed Week 2 assignments

--- src/main/java/Q2PrintArray.java
+++ src/main/java/Q2PrintArray.java
@@ -1,18 +1,14 @@
 public class Q2PrintArray {
     private static int[] array = {1, 2, 3, 4, 5, 6, 7};
 
-    public static void main(String[] args) {
-        printRecursion(array.length);
-    }
-
-    private static void printRecursion(int i) {
-        // Base case: if i is 0, printed all elements
-        if (i == 0) {
-            return;
+        public static void main(String[] args) {
+            printRecursion(array.length);
         }
-        
-        // call printRecursion with i-1
-        printRecursion(i - 1);
-        System.out.println(array[array.length - i]);
-    }
+
+        public static void printRecursion(int i) {
+            if (i >= 1) {
+                printRecursion(i - 1);
+                System.out.print(array[i - 1] + ",");
+            }
+        }
 }
